897f9ec728070d8639ba2b8830fa5f74226489ed
ayuspark <lola.luyaowang@gmail.com>
2017-07-19 14:44:26 -0700

    Added form to take in new stand sales info

--- cookie_stand_object.js
+++ cookie_stand_object.js
@@ -1,10 +1,11 @@
 'use strict';
-
+//__VARIABLE SETUP________________________________________________
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
 var cookieStand = [];
 var salesTable = document.getElementById('sales');
+var newStandForm = document.getElementById('new_stand');
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //cookie stand constructor
 function CookieStand(name, minCustPerHr, maxCustPerHr, avgCookiesPerCust){
   this.name = name;
@@ -43,33 +44,36 @@ function CookieStand(name, minCustPerHr, maxCustPerHr, avgCookiesPerCust){
   this.calcTotalCookiesPerDay(); //invoke previous calc functions
 
   cookieStand.push(this); //gather stand objects into an array
+}//END of constructor
 
-  this.render = function(){
-    //create rows for each Cookie stand
-    var trEl = document.createElement('tr'); //put the name in the first column
-    var tdEl = document.createElement('td');
-    tdEl.textContent = this.name;
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Render table using the prototype
+CookieStand.prototype.render = function(){
+  //create rows for each Cookie stand
+  var trEl = document.createElement('tr'); //put the name in the first column
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.name;
+  trEl.appendChild(tdEl);
+
+  tdEl.style.backgroundColor = '#353328';
+  tdEl.style.color = '#D9BD7E'; //style the location column
+
+  for(var n = 0; n < hours.length; n++){
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.cookiesPerHr[n];
     trEl.appendChild(tdEl);
+  }//put cookies per hour into following cells
 
-    tdEl.style.backgroundColor = '#353328';
-    tdEl.style.color = '#D9BD7E'; //style the location column
+  var totalTdEl = document.createElement('td');
+  totalTdEl.textContent = this.totalCookiesPerDay;
+  trEl.appendChild(totalTdEl);
 
-    for(var n = 0; n < hours.length; n++){
-      tdEl = document.createElement('td');
-      tdEl.textContent = this.cookiesPerHr[n];
-      trEl.appendChild(tdEl);
-    }//put cookies per hour into following cells
+  totalTdEl.style.color = '#D9BD7E'; //style the total cookies
 
-    var totalTdEl = document.createElement('td');
-    totalTdEl.textContent = this.totalCookiesPerDay;
-    trEl.appendChild(totalTdEl);
+  salesTable.appendChild(trEl);
+};
 
-    totalTdEl.style.color = '#D9BD7E'; //style the total cookies
-
-    salesTable.appendChild(trEl);
-  };
-} //end of constructor
-
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //create Table Header
 function tableHeader(){
   //create FIRST table row
@@ -98,7 +102,6 @@ function tableHeader(){
   totalThEl.style.color = '#353328'; //style the Total table head
 }
 
-
 //iterate to create rows for EACH STAND
 function cookieStandRows(){
   for(var i = 0; i < cookieStand.length; i++){
@@ -106,11 +109,33 @@ function cookieStandRows(){
   }
 }
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//create new instance of CookieStand
 new CookieStand('1st and Pike', 23, 65, 6.3);
 new CookieStand('SeaTac Airport', 3, 24, 1.2);
 new CookieStand('Seattle Center', 11, 38, 3.7);
 new CookieStand('Capitol Hill', 20, 38, 2.3);
 new CookieStand('Alki', 2, 16, 4.6);
 
-tableHeader();
-cookieStandRows();
+//__FUNCTION DECLARATION________________________________________
+tableHeader();//make Table Head
+cookieStandRows();//populate table body
+
+function handleNewStandSubmit(event){
+  event.preventDefault();
+
+  if(!event.target.stand.value || !event.target.minCustPerHr.value || !event.target.maxCustPerHr.value || !event.target.avgCookiesPerCust.value) {
+    return alert('Fields cannot be empty!');
+  };
+  //gather data from form
+  var newStand = event.target.stand.value;
+  var newMinCustPerHr = event.target.minCustPerHr.value;
+  var newMaxCustPerhr = event.target.maxCustPerHr.value;
+  var newAvgCookiePerCust = event.target.avgCookiesPerCust.value;
+  //feed into constructor
+  var newLineInTable = new CookieStand(newStand, newMinCustPerHr, newMaxCustPerhr, newAvgCookiePerCust);
+  //render new line in table
+  newLineInTable.render();
+}
+
+newStandForm.addEventListener('submit', handleNewStandSubmit);
