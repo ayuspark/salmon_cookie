4141503503b0e2a943dbfd2dfbc522e531398fc7
ayuspark <lola.luyaowang@gmail.com>
2017-07-22 23:05:58 -0700

    finished salmon cookie proj

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var teeNav = document.getElementById('tee_nav');
 var articleTee = document.getElementById('tee');
 var buyCookieNav = document.getElementById('cookie_nav');
 var articleCookie = document.getElementById('cookie');
+var articleMain = document.getElementById('main');
 articleAboutUs.style.display = 'none';
 articleTee.style.display = 'none';
 articleCookie.style.display = 'none';
@@ -15,6 +16,7 @@ function clickOnAboutUsNav(){
   articleAboutUs.style.margin = '0px auto 0px auto';
   articleTee.style.display = 'none';
   articleCookie.style.display = 'none';
+  articleMain.style.display = 'none';
 }
 aboutUsNav.addEventListener('click', clickOnAboutUsNav);
 
@@ -24,6 +26,7 @@ function clickOnCookieNav(){
   articleCookie.style.margin = '0px auto 0px auto';
   articleAboutUs.style.display = 'none';
   articleTee.style.display = 'none';
+  articleMain.style.display = 'none';
 }
 buyCookieNav.addEventListener('click', clickOnCookieNav);
 
@@ -33,6 +36,7 @@ function clickOnTeeNav(){
   articleTee.style.margin = '0px auto 0px auto';
   articleAboutUs.style.display = 'none';
   articleCookie.style.display = 'none';
+  articleMain.style.display = 'none';
 }
 teeNav.addEventListener('click', clickOnTeeNav);
 
@@ -41,7 +45,9 @@ var staffNav = document.getElementById('staff');
 function clickOnStaffNav(){
   var password = prompt('Enter your staff access code!');
   if(password === 'staff') {
-    window.open(sales.html, '_blank');
+    window.open('sales.html', '_blank');
+  } else {
+    alert('Access denied. The passcode you entered doesn\'t match ours.');
   }
 }
 staffNav.addEventListener('click', clickOnStaffNav);
